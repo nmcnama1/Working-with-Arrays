df43a3d9f9c685614b8a969d20348adc01a5130b
Nancy McNamara <nmcnama1@nd.edu>
2016-10-10 18:01:49 -0400

    completed arrays assignment

---
+++ run.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+get '/' do
+  # hello
+  @varaible_for_view = 'Hi! I am a variable. the @ at the beginning will make it accessible in the erb/view file.'
+  @people = ["Jonathan", "Joel", "Jarrett"]
+  erb :index, layout: :main
+end
+
+get '/years' do
+  @years = Array (1995..Time.now.year)
+
+  # code!
+  erb :years, layout: :main
+end
+
+get '/states' do
+  # code!
+  @states = ['Montana', 'Florida', 'California', 'Texas', 'Michigan', 'Missouri', 'Oregon', 'Georgia', 'West Virginia', 'Virginia', 'Maryland']
+  @states.sort
+  @states.sort!
+  erb :states, layout: :main
+end
